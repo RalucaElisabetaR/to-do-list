e77f4f815a09a0acf661415897f38dfbd9624047
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-04-13 13:41:22 +0200

    pass data from website to server

---
+++ app.js
@@ -0,0 +1,32 @@
+//jshint esversion:6
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+let items = []
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    let today = new Date()
+
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+    }
+    let day = today.toLocaleDateString('en-US', options)
+
+    res.render('list', { kindOfDay: day, newListItems: items })
+})
+app.post('/', (req, res) => {
+    let item = req.body.newItem
+    items.push(item)
+
+    res.redirect('/')
+})
+
+app.listen(2000, () => {
+    console.log('Server started at port 2000')
+})
