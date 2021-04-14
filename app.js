c1ef7c79f046e61e7676a1421251425add9baf24
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-04-14 16:22:39 +0200

    create date module

--- app.js
+++ app.js
@@ -2,29 +2,24 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-
+// eslint-disable-next-line no-undef
+const date = require(__dirname + '/date.js')
 const app = express()
-let items = []
-let workItems = []
+
+const items = []
+const workItems = []
+
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 // new route
 app.get('/', (req, res) => {
-    let today = new Date()
-
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long',
-    }
-    let day = today.toLocaleDateString('en-US', options)
-
+    let day = date.getDate()
     res.render('list', { listTitle: day, newListItems: items })
 })
 app.post('/', (req, res) => {
-    let item = req.body.newItem
+    const item = req.body.newItem
 
     if (req.body.list == 'Work List') {
         workItems.push(item)
@@ -42,10 +37,14 @@ app.get('/work', (req, res) => {
     res.render('list', { listTitle: 'Work List', newListItems: workItems })
 })
 app.post('/work', (req, res) => {
-    let item = req.body.newItem
+    const item = req.body.newItem
 
     workItems.push(item)
     res.redirect('/work')
+})
+// new route
+app.get('/about', (req, res) => {
+    res.render('about')
 })
 
 app.listen(2000, () => {
